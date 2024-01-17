b0baec78d2e9560cb0baff843a24d3e551e34cb1
Silver Sheldon <47248916+SilverSheldon@users.noreply.github.com>
2024-01-17 23:50:03 +0300

    Update app.js

--- app.js
+++ app.js
@@ -18,9 +18,18 @@ let file_v = file.files[0];
 
 // Функция обновления состояния главной кнопки
 function updateMainButtonState(e, type) {
+    console.log(e.target);
     if (type === "title") title_v = e.target.value;
     if (type === "content") content_v = e.target.value;
-    if (type === "file") file_v = e.target.files[0];
+    if (type === "file") {
+        const reader = new FileReader();
+        reader.readAsDataURL(e.target.files[0]);
+        reader.onload = (e) => {
+          console.log(e.target.result);
+          file_v = e.target.result;
+        };
+    }
+    
     if (!title_v || !content_v || !file_v) {
         tg.MainButton.hide(); // Скрываем кнопку, если поля не заполнены
     } else {
@@ -45,29 +54,16 @@ file.addEventListener("change", (e) => updateMainButtonState(e, "file"));
 
 // Обработчик клика по главной кнопке
 Telegram.WebApp.onEvent("mainButtonClicked", function() {
-    const formData = new FormData;
-    formData.append("title", title_v);
-    formData.append("content", content_v);
-    formData.append("file", file_v);
-    var object = {};
-    formData.forEach(function(value, key){
-        object[key] = value;
-    });
-    var json = JSON.stringify(object);
 
     console.log("HERE!!");
     if (title.value && content.value && file.files[0]) {
-        // let result = {
-        //     title: title.value,
-        //     content: content.value,
-        //     file_info: {
-        //         name: file.files[0].name,
-        //         type: file.files[0].type,
-        //         size: file.files[0].size
-        //     }
-        // };
+        let result = {
+            title: title.value,
+            content: content.value,
+            file_info: file_v
+        };
         // Отправляем данные в бота
-        tg.sendData(json);
+        tg.sendData(JSON.stringify(result));
         tg.close();
     }
   });
